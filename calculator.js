a12286d439f71ad17d33f8f44c31cc9bd00be58a
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-17 10:29:02 +0700

    added isNumber function

--- calculator.js
+++ calculator.js
@@ -7,11 +7,11 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num2 === 0 ? "Cannot divide by zero" : num1 / num2;
 // Step 02: Create a Validation Function for Inputs
 function validateInputs(num1, num2) {
-    if ( typeof num1 !== 'number' || isNaN(num1) ) return "Both inputs must be numbers";
-    if ( typeof num2 !== 'number' || isNaN(num2) ) return "Both inputs must be numbers";
+    if ( isNumber(num1) && isNumber(num2) ) return null;
 
-    return null;
+    return "Both inputs must be numbers";
 }
+const isNumber = num => typeof num === 'number' && !isNaN(num) ? true : false;
 // Step 03: Create a Calculator Function to Combine the Above Functions
 function calculator(num1, num2, operation) {
     if ( validateInputs(num1, num2) !== null )
@@ -35,7 +35,7 @@ function calculator(num1, num2, operation) {
 const num1 = 10;
 const num2 = 'a';    // 0, 'a'
 const operationList = ['add', 'subtract', 'multiply', 'divide'];
-const operation = operationList[0];
+const operation = operationList[3];  // 'unknown'
 
 console.log( '-'.repeat(30) );
 console.log( `${num1} + ${num2} = ${add(num1, num2)}` );
@@ -44,8 +44,8 @@ console.log( `${num1} * ${num2} = ${multiply(num1, num2)}` );
 console.log( `${num1} / ${num2} = ${divide(num1, num2)}` );
 console.log( '-'.repeat(30) );
 
-console.log( `Validate inputs ${num1} and ${num2}: ${validateInputs(num1, num2)}` );
+console.log( `Validate inputs ${num1} and ${num2} -> ${validateInputs(num1, num2)}` );
 console.log( '-'.repeat(30) );
 
-console.log( `Calculate ${num1} ${operation} ${num2} equals ${calculator(num1, num2, operation)}` );
+console.log( `Calculate ${num1} ${operation} ${num2} -> ${calculator(num1, num2, operation)}` );
 console.log( '-'.repeat(30) );
