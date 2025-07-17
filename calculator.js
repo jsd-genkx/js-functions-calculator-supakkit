2d54b8bed4ee1a0c30546747cad7ff3e87744a03
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-17 09:56:06 +0700

    changed validateInputs condition

--- calculator.js
+++ calculator.js
@@ -7,10 +7,10 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num2 === 0 ? "Cannot divide by zero" : num1 / num2;
 // Step 02: Create a Validation Function for Inputs
 function validateInputs(num1, num2) {
-    if ( typeof num1 === 'number' && !isNaN(num1) ) return null;
-    if ( typeof num2 === 'number' && !isNaN(num2) ) return null;
+    if ( typeof num1 !== 'number' || isNaN(num1) ) return "Both inputs must be numbers";
+    if ( typeof num2 !== 'number' || isNaN(num2) ) return "Both inputs must be numbers";
 
-    return "Both inputs must be numbers";
+    return null;
 }
 // Step 03: Create a Calculator Function to Combine the Above Functions
 function calculator(num1, num2, operation) {
